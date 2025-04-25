34e9d6743e17afbc7ebcad984ac99afa60e9bc42
Aaron <i-aaron.philip@devrev.ai>
2025-04-25 23:54:38 +0530

    Changed connection to use clickhouse cloud instead of clickhouse

--- database/connection.py
+++ database/connection.py
@@ -1,12 +1,26 @@
+# import clickhouse_connect
+
+# def get_db_client():
+#     return clickhouse_connect.get_client(
+#         host='localhost',
+#         port=8123,
+#         username='default',
+#         password='',
+#         database='default'
+#     )
+
+# client = get_db_client() 
+# # nvYid9.YcRPRU
 import clickhouse_connect
 
 def get_db_client():
     return clickhouse_connect.get_client(
-        host='localhost',
-        port=8123,
+        host='wu44ehj45r.asia-southeast1.gcp.clickhouse.cloud',
+        port=8443,
         username='default',
-        password='',
-        database='default'
+        password='nvYid9.YcRPRU',
+        database='default',
+        secure=True
     )
 
-client = get_db_client() 
+client = get_db_client()
